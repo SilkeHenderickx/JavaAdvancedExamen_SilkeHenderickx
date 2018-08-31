50d2a95f2ac99e53d0cfe2242dff7d419c89bbb6
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-08-31 13:42:57 +0200

    Question 2: all 4 answered.

--- SPRING-EXAMEN-JAVA2-VRAAG2/src/main/java/edu/ap/spring/Exam.java
+++ SPRING-EXAMEN-JAVA2-VRAAG2/src/main/java/edu/ap/spring/Exam.java
@@ -16,23 +16,37 @@ public class Exam {
 	// getallen terug te geven die deelbaar zijn door twee
 	// 2 punten
 	public int[] getTimesTwo(int[] numbers) {
+		return Arrays.stream(numbers)
+				.filter(n -> n % 2 == 0)
+				.toArray();
 	}
 	
 	// Maak gebruik van lambdas en streams om alle uppercase characters
 	// te tellen in de gegeven string
 	// 1 punt
 	public int countUppercaseCharacters(String sentence) {
+		return (int)sentence.chars()
+				.map(i -> (char) i)
+				.filter(Character::isUpperCase)
+				.count();
 	}
 	
 	// Maak gebruik van lambdas en streams om het aantal keren te tellen
 	// dat string s voorkomt (zowel uppercase als lowercase) in de lijst names
 	// 1 punt
 	public int countChar(List<String> names, String s) {
+		return (int)names.stream()
+				.filter(n -> n.toUpperCase() == s.toUpperCase())
+				.count();
 	}
 	
 	// Maak gebruik van lambdas en streams om het hoogste Y coordinaat
 	// te vinden
 	// 2 punten
 	public int getMaxY(List<Point> points) {
+		return points.stream()
+				.mapToInt(p -> p.y)
+				.max().orElseThrow(NoSuchElementException::new);
+
 	}	
 }
