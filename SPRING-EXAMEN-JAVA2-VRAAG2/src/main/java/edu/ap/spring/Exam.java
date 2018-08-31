f3d53820294c99e5b57dab54fa9f775581e43e2f
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-08-31 14:59:23 +0200

    Question 2: reworks

--- SPRING-EXAMEN-JAVA2-VRAAG2/src/main/java/edu/ap/spring/Exam.java
+++ SPRING-EXAMEN-JAVA2-VRAAG2/src/main/java/edu/ap/spring/Exam.java
@@ -16,37 +16,59 @@ public class Exam {
 	// getallen terug te geven die deelbaar zijn door twee
 	// 2 punten
 	public int[] getTimesTwo(int[] numbers) {
-		return Arrays.stream(numbers)
+		int[] result = Arrays.stream(numbers)
 				.filter(n -> n % 2 == 0)
 				.toArray();
+
+		/*for (int r:
+			 result) {
+			System.out.println(r);
+		}
+		*/
+		return result;
 	}
 	
 	// Maak gebruik van lambdas en streams om alle uppercase characters
 	// te tellen in de gegeven string
 	// 1 punt
 	public int countUppercaseCharacters(String sentence) {
-		return (int)sentence.chars()
+		int result = (int)sentence.chars()
 				.map(i -> (char) i)
 				.filter(Character::isUpperCase)
 				.count();
+		//System.out.println(result);
+		return result;
 	}
 	
 	// Maak gebruik van lambdas en streams om het aantal keren te tellen
 	// dat string s voorkomt (zowel uppercase als lowercase) in de lijst names
 	// 1 punt
 	public int countChar(List<String> names, String s) {
-		return (int)names.stream()
-				.filter(n -> n.toUpperCase() == s.toUpperCase())
+		//int r = (int)names.stream()
+		//		.map(n -> n.toUpperCase().contains(s.toUpperCase()));
+
+		char x = Character.toUpperCase(s.charAt(0));
+
+		String concatStrings = names.stream()
+				.map(n -> n.toUpperCase())
+				.reduce("", String::concat);
+		//System.out.println(concatStrings);
+		Integer result = (int)concatStrings.chars()
+				.filter(c -> c == x )
 				.count();
+
+		//System.out.println(result);
+		return result;
 	}
 	
 	// Maak gebruik van lambdas en streams om het hoogste Y coordinaat
 	// te vinden
 	// 2 punten
 	public int getMaxY(List<Point> points) {
-		return points.stream()
+		int result = points.stream()
 				.mapToInt(p -> p.y)
 				.max().orElseThrow(NoSuchElementException::new);
-
+		//System.out.println(result);
+		return result;
 	}	
 }
